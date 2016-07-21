2d08a6c299dca0a2e8b2601708ebdcae40c09c24
S-TamLTM <iosblurgroup@gmail.com>
2016-07-21 11:53:03 -0700

    add new file StudentListTableViewCell

---
+++ StudentRealm/Controller/UITableViewCell/StudentListTableViewCell/StudentListTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  StudentListTableViewCell.swift
+//  StudentRealm
+//
+//  Created by Le Tam on 7/21/16.
+//  Copyright © 2016 Asiantech. All rights reserved.
+//
+
+import UIKit
+
+class StudentListTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak private var lastNameLabel: UILabel!
+    @IBOutlet weak private var ageLabel: UILabel!
+    @IBOutlet weak private var sexLabel: UILabel!
+    @IBOutlet weak private var classLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        selectionStyle = .None
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func congiureStudentListCell(student: Student) {
+        lastNameLabel.text = NSString(format: "\(Strings.lastName)%@", student.studentName) as String
+        ageLabel.text = NSString(format: "\(Strings.age)%@", student.age) as String
+        sexLabel.text = NSString(format: "\(Strings.sex)%@", student.sex) as String
+        classLabel.text = NSString(format: "\(Strings.classRoom)%@", student.classRoom) as String
+        print("\(student.classRoom)")
+    }
+}
+	
--- StudentRealm/ViewController/Student/StudentViewController.swift
+++ StudentRealm/ViewController/Student/StudentViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import RealmSwift
 
 class StudentViewController: UIViewController {
 
+    @IBOutlet weak private var tableView: UITableView!
+    
+    var student: Results<Student>?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureStudentViewController()
+        setUpUI()
+        setUpData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,5 +33,48 @@ class StudentViewController: UIViewController {
     @IBAction func addStudentButton(sender: AnyObject) {
         let addStudentVC = AddStudentViewController()
         navigationController?.pushViewController(addStudentVC, animated: true)
+    }
+    
+    //MARK: Config
+    private func configureStudentViewController() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.registerNib(UINib(nibName: "StudentListTableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
+    }
+    
+    //MARK: Set Up Data
+    private func setUpData() {
+        studentList()
+    }
+    
+    //MARK: Set Up UI
+    private func setUpUI() {
+    
+    }
+    
+    private func studentList() {
+        do {
+            let realm = try Realm()
+            student = realm.objects(Student)
+        } catch {}
+    }
+}
+
+extension StudentViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 120
+    }
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return student!.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let studentCell = tableView.dequeueReusableCellWithIdentifier("Cell") as! StudentListTableViewCell
+        studentCell.congiureStudentListCell(student![indexPath.row])
+        return studentCell
     }
 }
